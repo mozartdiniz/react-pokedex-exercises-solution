d3ef63220db3b5b1d00f197cc47b7e409e478eb4
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-06-05 12:15:11 +0100

    Week 3 Exercise F completed

--- src/CaughtPokemon.js
+++ src/CaughtPokemon.js
@@ -1,17 +1,35 @@
 import React, { useState } from "react";
 
 function CaughtPokemon(props) {
-  const [totalCaught, setTotalCaught] = useState(0);
+  const [totalCaught, setTotalCaught] = useState([]);
+  const [pokemonNameInput, setPokemonNameInput] = useState("");
 
-  function incrementTotal() {
-    setTotalCaught(totalCaught + 1);
+  function catchPokemon() {
+    if (!pokemonNameInput.length) return;
+
+    setTotalCaught(totalCaught.concat(pokemonNameInput));
+    setPokemonNameInput("");
+  }
+
+  function handleInputChange(event) {
+    setPokemonNameInput(event.target.value);
   }
 
   return (
-    <p>
-      Caught {totalCaught} Pokemon on {props.date}
-      <button onClick={incrementTotal}>Catch Pokemon</button>
-    </p>
+    <div>
+      Caught Pokemon on {props.date}:
+      <ul>
+        {totalCaught.map((caughtPokemon, index) => {
+          return <li key={index}>{caughtPokemon}</li>;
+        })}
+      </ul>
+      <input
+        type="text"
+        value={pokemonNameInput}
+        onChange={handleInputChange}
+      />
+      <button onClick={catchPokemon}>Catch Pokemon</button>
+    </div>
   );
 }
 
