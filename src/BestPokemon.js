60857cdeec2357dc6b711800382ee8f8225c1e7b
Alasdair Smith <ali@alasdairsmith.co.uk>
2020-06-05 12:05:42 +0100

    Week 3 Exercise C completed

--- src/BestPokemon.js
+++ src/BestPokemon.js
@@ -13,18 +13,37 @@ const BestPokemon = (props) => {
   );
 };
 
-function BestPokemonFetcher() {
+function BestPokemonFetcher(props) {
   const [bestPokemon, setBestPokemon] = useState(null);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/1/")
+    fetch(`https://pokeapi.co/api/v2/pokemon/${props.pokemonId}/`)
       .then((res) => res.json())
       .then((data) => {
         setBestPokemon(data);
       });
-  }, []);
+  }, [props.pokemonId]);
 
   return bestPokemon ? <BestPokemon pokemon={bestPokemon} /> : null;
 }
 
-export default BestPokemonFetcher;
+function BestPokemonSelector() {
+  const [pokemonId, setPokemonId] = useState(null);
+
+  function handleBulbasaurClick() {
+    setPokemonId(1);
+  }
+  function handleCharmanderClick() {
+    setPokemonId(4);
+  }
+
+  return (
+    <div>
+      <button onClick={handleBulbasaurClick}>Fetch Bulbasaur</button>
+      <button onClick={handleCharmanderClick}>Fetch Charmander</button>
+      {pokemonId ? <BestPokemonFetcher pokemonId={pokemonId} /> : null}
+    </div>
+  );
+}
+
+export default BestPokemonSelector;
