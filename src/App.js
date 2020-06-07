30442ab7b6eb501e387fb85a02ec8a35195336a1
Mozart Diniz <mozart.diniz@gmail.com>
2020-06-07 09:33:59 +0100

    making it more dynamic

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
 import React from "react";
-
-import Logo from "./Logo";
 import BestPokemon from "./BestPokemon";
 import CaughtPokemon from "./CaughtPokemon";
 
@@ -8,13 +6,8 @@ function App() {
   const abilities = ["Anticipation", "Adaptability", "Run-Away"];
   const date = new Date().toLocaleDateString();
 
-  function logWhenClicked() {
-    console.log("Clicked!");
-  }
-
   return (
     <div>
-      <Logo appName="Pokedex" handleClick={logWhenClicked} />
       <BestPokemon abilities={abilities} />
       <CaughtPokemon date={date} />
     </div>
--- src/BestPokemon.js
+++ src/BestPokemon.js
@@ -1,5 +1,38 @@
 import React, { useState, useEffect } from "react";
 
+function BestPokemonSelector() {
+  const [pokemons, setPokemons] = useState([]);
+  const [selectedPokemonURL, setSelectedPokmemonURL] = useState(null);
+
+  useEffect(() => {
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=100`)
+      .then((res) => res.json())
+      .then((data) => {
+        setPokemons(data.results);
+      });
+  }, []);
+
+  function handleChange(e) {
+    setSelectedPokmemonURL(e.target.value);
+  }
+
+  return (
+    <div>
+      Select Pokemon:
+      <select onChange={handleChange}>
+        {pokemons.map((pokemon) => (
+          <option value={pokemon.url} key={pokemon.url}>
+            {pokemon.name}
+          </option>
+        ))}
+      </select>
+      {selectedPokemonURL ? (
+        <BestPokemonFetcher pokemonURL={selectedPokemonURL} />
+      ) : null}
+    </div>
+  );
+}
+
 const BestPokemon = (props) => {
   return (
     <div>
@@ -9,22 +42,56 @@ const BestPokemon = (props) => {
           return <li key={index}>{ability.ability.name}</li>;
         })}
       </ul>
+
+      <div>
+        <h5>Sprites:</h5>
+
+        {props.pokemon.sprites.back_default ? (
+          <img src={props.pokemon.sprites.back_default} alt="Back Default" />
+        ) : null}
+        {props.pokemon.sprites.back_female ? (
+          <img src={props.pokemon.sprites.back_female} alt="Back Female" />
+        ) : null}
+        {props.pokemon.sprites.back_shiny ? (
+          <img src={props.pokemon.sprites.back_shiny} alt="Back Shiny" />
+        ) : null}
+        {props.pokemon.sprites.back_shiny_female ? (
+          <img
+            src={props.pokemon.sprites.back_shiny_female}
+            alt="Back Shiny Female"
+          />
+        ) : null}
+        {props.pokemon.sprites.front_default ? (
+          <img src={props.pokemon.sprites.front_default} alt="Front Default" />
+        ) : null}
+        {props.pokemon.sprites.front_shiny ? (
+          <img src={props.pokemon.sprites.front_shiny} alt="Back Shiny" />
+        ) : null}
+        {props.pokemon.sprites.front_shiny_female ? (
+          <img
+            src={props.pokemon.sprites.front_shiny_female}
+            alt="Front Shiny Female"
+          />
+        ) : null}
+      </div>
     </div>
   );
 };
 
-function BestPokemonFetcher() {
+function BestPokemonFetcher(props) {
   const [bestPokemon, setBestPokemon] = useState(null);
 
   useEffect(() => {
-    fetch("https://pokeapi.co/api/v2/pokemon/1/")
+    if (!props.pokemonURL) {
+      return;
+    }
+
+    fetch(props.pokemonURL)
       .then((res) => res.json())
-      .then((data) => {
-        setBestPokemon(data);
-      });
-  }, []);
+      .then((data) => setBestPokemon(data));
+  }, [props.pokemonURL]);
 
   return bestPokemon ? <BestPokemon pokemon={bestPokemon} /> : null;
 }
 
-export default BestPokemonFetcher;
+export default BestPokemonSelector;
